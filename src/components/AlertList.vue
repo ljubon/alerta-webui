7667d9dba508a4eeee6181f58db4913e1ace577c
headphonejames <generalfuzz@gmail.com>
2023-01-30 22:54:09 -0800

    Allow multiple alerts to be selected in the alert list. Allow a single note to be applied to all selected alerts. Add hyperlink fields to alert for operational runbooks.

--- src/components/AlertList.vue
+++ src/components/AlertList.vue
@@ -85,6 +85,19 @@
             >
               {{ props.item.event }}
             </span>
+
+            <!-- Additional column for operational runbook links -->
+            <span
+              v-if="col == 'info'"
+            >
+              <div
+                v-for="data in $config.runbook"
+                :key="data.output"
+              >                
+                <div v-html="findMatch(data,props)" />
+              </div>
+            </span>
+            
             <span
               v-if="col == 'environment'"
             >
@@ -497,6 +510,7 @@ export default {
       id: { text: i18n.t('AlertId'), value: 'id' },
       resource: { text: i18n.t('Resource'), value: 'resource' },
       event: { text: i18n.t('Event'), value: 'event' },
+      info: { text: i18n.t('Info'), value: 'info', sortable: false }, // Column to show operational runbook info.
       environment: { text: i18n.t('Environment'), value: 'environment' },
       severity: { text: i18n.t('Severity'), value: 'severity' },
       correlate: { text: i18n.t('Correlate'), value: 'correlate' },
@@ -526,7 +540,8 @@ export default {
     details: false,
     selectedId: null,
     multiselect: false,
-    timer: null
+    timer: null,
+    shiftDown: false
   }),
   computed: {
     displayDensity() {
@@ -548,8 +563,8 @@ export default {
     },
     columnWidths() {
       return {
-        '--value-width': this.valueWidth() + 'px',
-        '--text-width': this.textWidth() + 'px'
+        // '--value-width': this.valueWidth() + 'px',
+        // '--text-width': this.textWidth() + 'px' // altered to prevent overflow and odd spacing in last column
       }
     },
     isLoading() {
@@ -577,8 +592,7 @@ export default {
     },
     customHeaders() {
       return this.$config.columns.map(c =>
-        this.headersMap[c] || { text: this.$options.filters.capitalize(c), value: 'attributes.' + c }
-      )
+        this.headersMap[c] || { text: this.$options.filters.capitalize(c), value: 'attributes.' + c } )
     },
     selectedItem() {
       return this.alerts.filter(a => a.id == this.selectedId)[0]
@@ -591,6 +605,34 @@ export default {
         return this.$store.state.alerts.selected
       },
       set(value) {
+        const alerts = this.$store.state.alerts.alerts
+        if (this.shiftDown) {
+          // check if there is a gap between selected and previously
+          let indexes = []
+          for (let i = 0; i < value.length; i++) {
+            const alertIndex = alerts.findIndex((x => x.id === value[i].id))
+            indexes.push(alertIndex)
+            if (i > 0) {
+              if (Math.abs(alertIndex - indexes[i - 1]) > 1) {
+                const lowIndex = Math.min(alertIndex, indexes[i - 1])
+                const highIndex = Math.max(alertIndex, indexes[i - 1])
+                // fill in all the missing alerts
+                for (let j = lowIndex + 1; j < highIndex; j++) {
+                  // check if alert is already in the selected list
+                  let foundAlert = false
+                  value.forEach( alert => {
+                    if( alert.id === alerts[j].id) {
+                      foundAlert = true
+                    }
+                  } )
+                  if (!foundAlert) {
+                    value.push(alerts[j])
+                  }
+                }
+              }
+            }
+          }
+        }
         this.$store.dispatch('alerts/updateSelected', value)
       }
     },
@@ -609,7 +651,35 @@ export default {
       this.pagination = Object.assign({}, this.pagination, {rowsPerPage: val})
     }
   },
+  created() {
+    this.initHotkeys()
+  },
   methods: {
+    initHotkeys() {
+      window.addEventListener('keydown', this.processHotkey)
+      window.addEventListener('keyup', this.removeHotkey)
+    },
+    processHotkey(event) {
+      event.code === 'ShiftLeft' || event.code === 'ShiftRight' ? this.shiftDown = true : 0
+    },
+    removeHotkey(event) {
+      event.code === 'ShiftLeft' || event.code === 'ShiftRight' ? this.shiftDown = false : 0
+    },
+    // method for mapping table data to links from runbook data
+    findMatch(additionalRespObj, props){
+      const validMatch = additionalRespObj.matches.every(matchesObj => {
+        // make comparisons case-insensitive
+        const filter = new RegExp((matchesObj.regex).toLowerCase())
+        const columnName = (matchesObj.column).toLowerCase()
+        const columnData = (props.item[columnName]).toLowerCase()
+        return filter.test(columnData)
+      })
+      // return link if all regex checks pass
+      return validMatch ? additionalRespObj.output : null
+    },
+    attributeMatch(item){
+      return this.cars.id === carID ? true : false
+    },
     duration(item) {
       return moment.duration(moment().diff(moment(item.receiveTime)))
     },
@@ -812,7 +882,7 @@ div.select-box {
 }
 
 div.action-buttons {
-  position: absolute;
+  position: relative;
   opacity: 0;
   right: 0;
   top: 0.5em;
